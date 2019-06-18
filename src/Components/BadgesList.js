92d543eaf79ccd0d67175d687fbec7245b3d9e05
ingrid <msagrerom@inee.edu.mx>
2019-06-18 18:10:51 -0500

    post nuevos badges y update de los existentes

--- src/Components/BadgesList.js
+++ src/Components/BadgesList.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom';
+
 import"./styles/BadgesList.css"
 import twLogo from '../images/twitter.svg';
 import Gravatar from '../Components/Gravatar'
@@ -23,28 +23,28 @@ class BadgesList extends React.Component{
                {this.props.badges.map(badges => {
                    return (
                        <li key={badges.id} className= 'Badge__section-name'>
-                           <div className='BadgeList_badge-container'>
-                                <Gravatar
-                                    email ={badges.email}
-                                    alt='Avatar'
-                                    className='BadgeList__avatar'
-                                />
-                                 
-                            <div className='BadgeList_badge_text_container'>
-                                <p className='BadgeList__name'>
-                                    {badges.firstName} {badges.lastName}
-                                </p>
-                                <p>
-                                    <img src={twLogo} alt='logo de twitter' className='tw__logo'/>
-                                    <span className='tw__text'>{badges.twitter}</span>
-                                </p>
-                                <p>
-                                    {badges.jobTitle}
-                                </p>
-                                
-                            </div>
-                            
-                           </div>
+                           <Link className="text-reset text-decoration-none" to={`/badges/${badges.id}/edit`}>
+                                <div className='BadgeList_badge-container'>
+                                        <Gravatar
+                                            email ={badges.email}
+                                            alt='Avatar'
+                                            className='BadgeList__avatar'
+                                        />
+                                        
+                                    <div className='BadgeList_badge_text_container'>
+                                        <p className='BadgeList__name'>
+                                            {badges.firstName} {badges.lastName}
+                                        </p>
+                                        <p>
+                                            <img src={twLogo} alt='logo de twitter' className='tw__logo'/>
+                                            <span className='tw__text'>{badges.twitter}</span>
+                                        </p>
+                                        <p>
+                                            {badges.jobTitle}
+                                        </p>
+                                    </div>
+                                </div>
+                           </Link>
                        </li>
                    )
                })}
