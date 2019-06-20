d2a4f70a45728b45c7ddadf40fd9047cf2abfb7c
ingrid <msagrerom@inee.edu.mx>
2019-06-20 18:28:10 -0500

    trabajando con hooks para terminar el proyecto

--- src/Components/BadgesList.js
+++ src/Components/BadgesList.js
@@ -5,53 +5,99 @@ import"./styles/BadgesList.css"
 import twLogo from '../images/twitter.svg';
 import Gravatar from '../Components/Gravatar'
 
+/*custom hook que empaqueta toda la funcionalidad*/
+function useSearchBadges (badges) {
+    const [ query, setQuery ] = React.useState ('');
+    const [ filteredBadges, setFilteredBadges] = React.useState(badges)
 
-class BadgesList extends React.Component{
-    render(){
-        if(this.props.badges.length === 0){
+     React.useMemo(() => {
+        const result =badges.filter(badge => {
+        return `${badge.firstName} ${badge.lastName}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+    setFilteredBadges( result)
+}, [ badges, query ]);
+
+return { setQuery, filteredBadges }
+}
+
+function BadgesList (props){
+        const badges = props.badges;
+
+        const {query, setQuery, filteredBadges } = useSearchBadges(badges)
+
+        
+
+        if(filteredBadges.length === 0){
             return (
                 <div>
+                    <div className="form-group">
+                        <label> Filter Badges</label>
+                        <input 
+                            type="text"
+                            className="form-control" 
+                            value={query}
+                            onChange={e => {
+                            setQuery(e.target.value)
+                            }}
+                        />
+                    </div>
+                    
                     <h3>No badges were found!</h3>
                     <Link  className='btn btn-primary' to= '/badges/New'>
                         Create new badge
                     </Link>
                 </div>
-            )
+               
+            );
         }
         return(
-           <ul className='list-unstyled'>
-               {this.props.badges.map(badges => {
-                   return (
-                       <li key={badges.id} className= 'Badge__section-name'>
-                           <Link className="text-reset text-decoration-none" to={`/badges/${badges.id}/`}>
-                                <div className='BadgeList_badge-container'>
-                                        <Gravatar
-                                            email ={badges.email}
-                                            alt='Avatar'
-                                            className='BadgeList__avatar'
-                                        />
-                                        
-                                    <div className='BadgeList_badge_text_container'>
-                                        <p className='BadgeList__name'>
-                                            {badges.firstName} {badges.lastName}
-                                        </p>
-                                        <p>
-                                            <img src={twLogo} alt='logo de twitter' className='tw__logo'/>
-                                            <span className='tw__text'>{badges.twitter}</span>
-                                        </p>
-                                        <p>
-                                            {badges.jobTitle}
-                                        </p>
-                                    </div>
-                                </div>
-                           </Link>
-                       </li>
-                   )
-               })}
+            <div className="BadgesList">
+                <div className="form-group">
+                    <label> Filter Badges</label>
+                    <input type="text" className="form-control" 
+                        value={query}
+                        onChange={(e) => {
+                            setQuery(e.target.value)
+                        }}
+                    />
+                </div>
+                <ul className='list-unstyled'>
+                    {filteredBadges.map(badges => {
+                        return (
+                            <li key={badges.id} className= 'Badge__section-name'>
+                                <Link className='text-reset text-decoration-none' to={`/badges/${badges.id}/`}>
+                                        <div className='BadgeList_badge-container'>
+                                                <Gravatar
+                                                    email ={badges.email}
+                                                    alt='Avatar'
+                                                    className='BadgeList__avatar'
+                                                />
+                                                
+                                            <div className='BadgeList_badge_text_container'>
+                                                <p className='BadgeList__name'>
+                                                    {badges.firstName} {badges.lastName}
+                                                </p>
+                                                <p>
+                                                    <img src={twLogo} alt='logo de twitter' className='tw__logo'/>
+                                                    <span className='tw__text'>{badges.twitter}</span>
+                                                </p>
+                                                <p>
+                                                    {badges.jobTitle}
+                                                </p>
+                                            </div>
+                                        </div>
+                                </Link>
+                            </li>
+                        )
+                    })}
 
-           </ul>
-        )
-    }
+                </ul>
+            </div>
+           
+        );
+    
 }
 
 export default BadgesList;
--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 import './styles/BadgeDetails.css';
 import confLogo from '../images/platziconf-logo.svg';
 import Badge from '../Components/Badge';
+import DeleteBadgeModal from '../Components/DeleteBadgeModal'
+
+/*Custom hook
+function useIncreaseCount(max){
+  const [ count, setCount ] = React.useState(0)
+
+  if (count > max) {
+    setCount(0)
+  } 
+  return [count, setCount]; 
+}*/
+
 
 function BagdgeDetails (props) {
+   // const [ count, setCount ]= useIncreaseCount(4); hooks
     const badge = props.badge;
 return (
     <div>
@@ -35,18 +49,27 @@ return (
                         jobTitle = {badge.jobTitle}
                     />
                 </div>
-                <div className='col'>
+                <div className='col'> 
                     <h2>Actions</h2>
                     <div>
                         <div> 
+                          {/* el botón que controla la acción del hook
+                          <button onClick={()=>{
+                            setCount(count +1);
+                          }}className='btn btn-primary mr-4'>
+                            Increse Count : {count}
+                        </button>*/}
                             <Link className='btn btn-primary mb-4' to={`/badges/${badge.id}/edit`}>
                                 Edit
                             </Link>
                         </div>
                         <div> 
-                            <button className='btn btn-danger'>
-                                Delete
-                            </button>
+                            <button onClick={props.onOpenModal} className='btn btn-danger'>Delete</button>
+                            <DeleteBadgeModal 
+                              isOpen={props.modalIsOpen} 
+                              onClose={props.onCloseModal}
+                              onDeleteBadge={props.onDeleteBadge}
+                            />  
                         </div>
                     </div>
                 </div>
